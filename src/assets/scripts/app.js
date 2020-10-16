aad699171389dff597f0c5f2da48f6c25e12d9ee
bchiang7 <brittany.chiang@gmail.com>
2020-10-16 12:27:35 -0400

    Add Everett trial fonts and comment out unused JS for now

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -2,22 +2,22 @@ import 'babel-polyfill';
 
 import { onDocumentReady } from './utils';
 import ExternalLinks from './ExternalLinks';
-import Search from './Search';
-import SearchBar from './SearchBar';
-import SearchPage from './SearchPage';
-import ThemeToggle from './ThemeToggle';
+// import Search from './Search';
+// import SearchBar from './SearchBar';
+// import SearchPage from './SearchPage';
+// import ThemeToggle from './ThemeToggle';
 
 onDocumentReady(() => {
   new ExternalLinks();
-  new ThemeToggle();
+  // new ThemeToggle();
 
-  const search = new Search();
+  // const search = new Search();
 
-  if (document.querySelector('.js-search-input')) {
-    new SearchBar(search);
-  }
+  // if (document.querySelector('.js-search-input')) {
+  //   new SearchBar(search);
+  // }
 
-  if (document.querySelector('.search')) {
-    new SearchPage(search);
-  }
+  // if (document.querySelector('.search')) {
+  //   new SearchPage(search);
+  // }
 });
