230c996a3de87a861a2c381623ebb0abf40c9c7e
Alec Marcus <alec@alecmarc.us>
2020-11-04 11:07:00 -0500

    printjs: re-organize, add hover option, add cleanup func

--- src/assets/scripts/Print/index.js
+++ src/assets/scripts/Print/index.js
@@ -8,13 +8,16 @@ class Print {
     this.lines = printEl.getAttribute('data-print').split('/n/');
 
     // Parse animation config options from data attributes
-    this.initialDelay = parseInt(printEl.getAttribute('data-initial-delay')) || 0;
-    this.visibleFor = parseInt(printEl.getAttribute('data-visible-for')) || 10000;
-    this.invisibleFor = parseInt(printEl.getAttribute('data-invisible-for')) || 2000;
-    this.targetLoopCount = parseInt(printEl.getAttribute('data-loops')) || 'infinite';
     this.fillMode = printEl.getAttribute('data-fill-mode') || 'forwards';
+    this.initialDelay = parseInt(printEl.getAttribute('data-initial-delay')) || 0;
+    this.invisibleFor = parseInt(printEl.getAttribute('data-invisible-for')) || 2000;
+    this.visibleFor = parseInt(printEl.getAttribute('data-visible-for')) || 10000;
+    this.useHover = printEl.getAttribute('data-use-hover') === ('true' || '1');
+    this.targetLoopCount = this.useHover
+      ? 'infinite'
+      : parseInt(printEl.getAttribute('data-loops')) || 'infinite';
 
-    // Set static animation config options
+    // Delay between characters — "typing" speed
     this.pauseMin = 60;
     this.pauseMax = 120;
 
@@ -24,7 +27,7 @@ class Print {
     this.lineElsFragment = new DocumentFragment();
 
     this.baseCharEl = document.createElement('span');
-    this.baseCharEl.classList.add('js-print-char');
+    this.baseCharEl.classList.add('js-print-char', this.useHover && 'js-print-shown');
 
     // Create empty array to store char DOM elements
     this.charEls = [];
@@ -33,24 +36,29 @@ class Print {
 
     this.isIdling = false;
     this.idleTimeout = 60000;
+    this.throttledResetIdle = throttle(() => this.resetIdle(), 10000);
+
+    this.hoverEvents = ['mouseenter', 'touchstart', 'focus'];
 
     this.init();
   }
 
   init() {
-    // Setup initial idle timeouts and reset listeners
-    document.addEventListener(
-      'mousemove',
-      throttle(() => this.resetIdle(), 10000),
-    );
-    document.addEventListener(
-      'keyup',
-      throttle(() => this.resetIdle(), 10000),
-    );
-    setTimeout(() => {
-      this.isIdling = true;
-    }, this.idleTimeout);
+    this.setupDOM();
 
+    if (this.useHover) {
+      this.setupHover();
+    } else {
+      this.setupIdle();
+      this.loop();
+    }
+  }
+
+  /**
+   * DOM logic
+   *****************************************/
+
+  setupDOM() {
     // Render markup for each line
     this.lines.forEach(line => {
       const lineEl = this.baseLineEl.cloneNode();
@@ -68,25 +76,36 @@ class Print {
 
     // Attach lines contianing characters to the instantiating DOM element
     this.printEl.appendChild(this.lineElsFragment);
-
-    // Begin the animation
-    this.loop();
   }
 
-  get shouldLoop() {
-    return (
-      !this.isIdling &&
-      (this.targetLoopCount === 'infinite' || this.loopCounter < this.targetLoopCount)
-    );
+  /**
+   * Hover logic
+   *****************************************/
+
+  setupHover() {
+    this.hoverEvents.forEach(event => {
+      this.printEl.addEventListener(
+        event,
+        throttle(() => {
+          this.hideCharacter(0);
+
+          setTimeout(() => {
+            this.showCharacter(0);
+          }, this.invisibleFor);
+        }, this.invisibleFor * 1.5),
+      );
+    });
+  }
+
+  /**
+   * Animation logic
+   *****************************************/
+
+  get allLoopsComplete() {
+    return this.targetLoopCount === 'infinite' ? false : this.loopCounter === this.targetLoopCount;
   }
 
   showCharacter(i) {
-    /**
-     * 1. Shows the character at charEls[i] after a timeout
-     * 2. Then calls this function with i + 1
-     */
-
-    // If no corresponding element exists, do nothing
     if (!this.charEls[i]) {
       return;
     }
@@ -96,42 +115,45 @@ class Print {
       i === 0 ? 0 : this.pauseMin + Math.floor(Math.random() * (this.pauseMax - this.pauseMin));
 
     setTimeout(() => {
+      // Show the character at charEls[i] after a timeout
       window.requestAnimationFrame(() => this.charEls[i].classList.add('js-print-shown'));
+
+      // Call this function with i + 1
       this.showCharacter(i + 1);
     }, pauseDur);
   }
 
   hideCharacter(i) {
-    /**
-     * 1. Hides the character at charEls[i] after a timeout
-     * 2. Then calls this function with i + 1
-     */
-
-    // If no corresponding element exists, do nothing
     if (!this.charEls[i]) {
       return;
     }
 
-    if (i === 0) {
+    // Enable automatic looping
+    if (i === 0 && !this.useHover) {
       this.loop();
     }
 
     setTimeout(() => {
-      window.requestAnimationFrame(() => this.charEls[i].classList.remove('js-print-shown'));
+      // Hide the character at charEls[i] after a timeout
+      window.requestAnimationFrame(() => {
+        this.charEls[i].classList.remove('js-print-shown');
+      });
+
+      // Call this function with i + 1
       this.hideCharacter(i + 1);
     }, this.pauseMin);
   }
 
   loop() {
-    if (this.shouldLoop || this.fillMode === 'forwards') {
+    if (!this.isIdling && !this.allLoopsComplete) {
       const timeout = this.loopCounter === 0 ? this.initialDelay : this.invisibleFor;
 
       setTimeout(() => {
         this.loopCounter++;
         this.showCharacter(0);
 
-        // End the loop with visible text if fill mode is set to 'forwards'
-        if (!this.shouldLoop && this.fillMode === 'forwards') {
+        if (this.isIdling || (this.allLoopsComplete && this.fillMode === 'forwards')) {
+          this.cleanupIdle();
           return;
         }
 
@@ -139,7 +161,24 @@ class Print {
           this.hideCharacter(0);
         }, this.visibleFor);
       }, timeout);
+    } else {
+      this.cleanupIdle();
     }
+  }
+
+  /**
+   * Idle logic
+   *****************************************/
+
+  setupIdle() {
+    this.windowIdleTimeout = () =>
+      window.setTimeout(() => {
+        this.isIdling = true;
+      }, this.idleTimeout);
+
+    document.addEventListener('mousemove', this.throttledResetIdle);
+    document.addEventListener('keyup', this.throttledResetIdle);
+    this.windowIdleTimeout();
   }
 
   resetIdle() {
@@ -156,6 +195,12 @@ class Print {
     } else {
       this.isIdling = false;
     }
+  }
+
+  cleanupIdle() {
+    document.removeEventListener('mousemove', this.throttledResetIdle);
+    document.removeEventListener('keyup', this.throttledResetIdle);
+    window.clearTimeout(this.windowIdleTimeout);
   }
 }
 
