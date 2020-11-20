5cc673b217bab78728474679cd0f0a4ebd9f8499
bchiang7 <brittany.chiang@gmail.com>
2020-11-20 17:30:17 -0500

    Fix last modified script to only change staged files

--- scripts/last_modified.sh
+++ scripts/last_modified.sh
@@ -1,11 +1,13 @@
-git diff --name-only HEAD | while read filename; do
-  if [[ "${filename##*.}" == "md" ]]; then
+# Get staged files
+git diff --name-only --cached HEAD | while read filename; do
+  # If the file exists and ends in `.md` then update the date field
+  if [[ -f "$filename" && "${filename##*.}" == "md" ]]; then
     last_modified=`date +"%Y-%m-%d"`
     sed -i "" "s|date:.*|date: $last_modified|g" "$filename"
+    echo "Updating timestamp for $filename"
+    git add $filename
   fi
 done
-
-git add .
 
 # This sets all .md files to have the same last edit dates as the Git log.
 # It shouldn't actually need to be run, but keeping it here for posterity.
