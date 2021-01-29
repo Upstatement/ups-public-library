41344e4782cdcc95b2893a9a13ea03d60b755aed
Alec Marcus <alec@alecmarc.us>
2021-01-29 11:00:13 -0500

    fix % complete calculation

--- src/assets/scripts/ProgressBar.js
+++ src/assets/scripts/ProgressBar.js
@@ -4,23 +4,36 @@ class ProgressBar {
   constructor() {
     this.entryContentEl = document.getElementById('js-entry-content');
     this.navigationEl = document.getElementById('js-entry-nav');
+    this.dingbatWrapper = document.getElementById('js-random-dingbat');
     this.progressIndicator = document.getElementById('js-progress-indicator');
-    this.initialY = this.entryContentEl.getBoundingClientRect().top;
-    this.onScrollThrottled = () => throttle(this.onScroll(), 10000);
+
+    this.setProgressBarWidthThrottled = () => throttle(this.setProgressBarWidth(), 500);
 
     this.init();
   }
 
-  init() {
-    document.addEventListener('scroll', this.onScrollThrottled, { passive: true });
+  get entryScrollBottom() {
+    return (
+      this.entryContentEl.scrollHeight -
+      window.innerHeight +
+      this.dingbatWrapper.scrollHeight +
+      this.navigationEl.scrollHeight
+    );
   }
 
-  onScroll() {
+  get percentComplete() {
+    return Math.min(100 * (window.scrollY / this.entryScrollBottom), 100);
+  }
+
+  setProgressBarWidth() {
     window.requestAnimationFrame(() => {
-      this.progressIndicator.style.width = `${
-        (100 * window.scrollY) / (this.entryContentEl.scrollHeight - window.innerHeight)
-      }vw`;
+      this.progressIndicator.style.width = `${this.percentComplete}vw`;
     });
+  }
+
+  init() {
+    document.addEventListener('scroll', this.setProgressBarWidthThrottled, { passive: true });
+    window.addEventListener('resize', this.setProgressBarWidthThrottled, { passive: true });
   }
 }
 
