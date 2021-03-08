fa87b5e0ebb0d7c1a72e2837aa89d9eb0935f477
Stephanie Lee <stephanie.lee@upstatement.com>
2021-03-08 14:45:25 -0800

    Grab all links in entries

--- src/assets/scripts/RotatingColor.js
+++ src/assets/scripts/RotatingColor.js
@@ -1,7 +1,7 @@
 class RotatingColor {
   constructor() {
     this.startingRed = 0;
-    this.startingGreen = 122;
+    this.startingGreen = 125;
     this.startingBlue = 255;
 
     this.redModifier = 1;
@@ -9,28 +9,34 @@ class RotatingColor {
     this.blueModifier = 1;
 
     setInterval(() => {
-      const element = document.querySelector('.md a');
-      element.style.color = `rgb(${this.startingRed}, ${this.startingGreen}, ${this.startingBlue})`;
+      const links = document.querySelectorAll('.md a');
+
+      links.forEach(el => {
+        el.style.color = `rgb(${this.startingRed}, ${this.startingGreen}, ${this.startingBlue})`;
+      });
+
       if (this.startingRed <= 0) {
         this.redModifier = 1;
       } else if (this.startingRed >= 255) {
         this.redModifier = -1;
       }
+
       if (this.startingGreen <= 0) {
         this.greenModifier = 1;
       } else if (this.startingGreen >= 255) {
         this.greenModifier = -1;
       }
+
       if (this.startingBlue <= 0) {
         this.blueModifier = 1;
       } else if (this.startingBlue >= 255) {
         this.blueModifier = -1;
       }
 
-      this.startingRed += 1 * this.redModifier;
-      this.startingGreen += 1 * this.greenModifier;
-      this.startingBlue += 1 * this.blueModifier;
-    }, 100);
+      this.startingRed += 3 * this.redModifier;
+      this.startingGreen += 5 * this.greenModifier;
+      this.startingBlue += 10 * this.blueModifier;
+    }, 150);
   }
 }
 
