71d9d178a7cb2192311d9467005ac584024c6f18
alecmarcus <github@alecmarc.us>
2021-04-25 14:23:32 -0400

    tweak where comments

--- src/_11ty/filters/where.js
+++ src/_11ty/filters/where.js
@@ -1,11 +1,14 @@
 const _ = require('lodash');
 
 /**
- * From Stackbit's nunjucks implementation, unibit.
- * Filters an array of objects by values using their path.
+ * Filters an array of objects given a path, test, and comparison operator.
  *
- * Source: https://github.com/stackbit/unibit/blob/2fb46d3834743b72a9faa0ff3d443f375c72ab81/src/filters.js#L95
- * Documentation: https://www.stackbit.com/docs/unibit/nunjucks-filters/#where_path_operator_match
+ * Implementing our own because the selectattr and removeattr filters don't work:
+ * @see https://github.com/11ty/eleventy/issues/910
+ *
+ * Adapted from Stackbit's nunjucks implementation, unibit.
+ * @see https://github.com/stackbit/unibit/blob/2fb46d3834743b72a9faa0ff3d443f375c72ab81/src/filters.js#L95
+ *
  * Usage:
  *
  * {{ array | where(key: String[, match: any, operator: (== | != | > | >= | < | <=)]) }}
